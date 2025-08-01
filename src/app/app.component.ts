ce1dbba95dbba6e8c98a3e06ac0df269de068d70
Isaac Ramirez <isaacrdmc@gmail.com>
2025-08-01 16:28:15 -0600

    Cambios minimos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+// import { LoginComponent } from './pages/login/login.component';
+// import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HomeComponent, NavbarComponent],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatButtonModule, NavbarComponent],
+  // imports: [RouterOutlet, LoginComponent, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+// import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    // CommonModule,
+    // RouterLink,
+    // MatSnackBarModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
+
+// ?
 export class NavbarComponent {
+
+  // // ?
+  // autService = inject(Sut)
+
+
 
 }
