d91d4a9ebb334faebc711ccfb340deb04f85fcb9
Isaac Ramirez <isaacrdmc@gmail.com>
2025-08-01 17:03:02 -0600

    Correcciónes del modulo home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 
 // ? Definimos las rutas de la aplicación
 export const routes: Routes = [
-  { path:'login', component:LoginComponent },
-  { path:'home', component:HomeComponent }
+  { path:'', component:HomeComponent },
+  { path:'login', component:LoginComponent }
 
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+// import { AuthService } from '../../services/auth.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatInputModule,
+    RouterLink,
+    MatSnackBarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
