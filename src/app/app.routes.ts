32ebb4cf11637fbb9a570546d48e126dc711f39a
Isaac Ramirez <isaacrdmc@gmail.com>
2025-08-02 01:03:07 -0600

    Creando el formualrio de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 // ? Definimos las rutas de la aplicación
 export const routes: Routes = [
   { path:'', component:HomeComponent },
-  { path:'login', component:LoginComponent }
+  { path:'login', component:LoginComponent },
+  { path:'register', component:RegisterComponent}
 
 ];
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { M } from "../../../../node_modules/@angular/material/form-field.d-CMA_QQ0R";
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Router } from 'express';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatSelectModule,
+    RouterLink,
+    MatSelectModule,
+    MatIconModule,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+
+// ^
+export class RegisterComponent implements OnInit{
+
+  // *
+  fb = inject(FormBuilder);
+  registerForm!:FormGroup;
+  router = inject(Router);
+  confirmPasswordHide: boolean | undefined;
+
+  // ^
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      fullName: ['', [Validators.required]],
+      roles: [],
+      confirmPassword: ['', [Validators.required]],
+    })
+  }
+
+
+
+}
